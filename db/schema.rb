039493ca5f6e6cd83db7f3125e01a14db9dec0e7
matsuhisa <matsuhisa+github@gmail.com>
2017-07-22 20:20:57 +0900

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170722111824) do
+ActiveRecord::Schema.define(version: 20170722112015) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
